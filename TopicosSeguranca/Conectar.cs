308ca8385fde2606ce63548f6d2a851d3554b804
eric <2025185438@my.ipleiria.pt>
2026-06-01 15:05:46 +0100

    atualizado o chat do cliente

--- TopicosSeguranca/Conectar.cs
+++ TopicosSeguranca/Conectar.cs
@@ -1,10 +1,7 @@
 ﻿using EI.SI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TopicosSeguranca
@@ -13,13 +10,20 @@ namespace TopicosSeguranca
     {
         TcpClient client;
         ProtocolSI protocolSI = new ProtocolSI();
-        NetworkStream networkStream;        
-        public void Conexao()
+        NetworkStream networkStream;
+
+        // Passamos o Form1 como parâmetro para conseguir atualizar a interface gráfica
+        public void Conexao(Form1 form)
         {
             try
             {
                 client = new TcpClient("127.0.0.1", 4000);
                 networkStream = client.GetStream();
+
+                // Cria e inicia a Thread de escuta do Servidor
+                Thread threadEscuta = new Thread(() => OuvirServidor(form));
+                threadEscuta.IsBackground = true; // Para fechar a thread junto com a aplicação
+                threadEscuta.Start();
             }
             catch (Exception e)
             {
@@ -28,13 +32,37 @@ namespace TopicosSeguranca
             }
         }
 
+        private void OuvirServidor(Form1 form)
+        {
+            ProtocolSI protocolReader = new ProtocolSI();
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = networkStream.Read(protocolReader.Buffer, 0, protocolReader.Buffer.Length);
+                    if (bytesRead <= 0) break;
+
+                    if (protocolReader.GetCmdType() == ProtocolSICmdType.DATA)
+                    {
+                        string msg = protocolReader.GetStringFromData();
+
+                        // Envia a mensagem recebida para a função segura do Form
+                        form.AdicionarMensagemNoChat(msg);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Conexão com o servidor perdida.");
+            }
+        }
+
         public void LerMensagem(string msg)
         {
             if (networkStream != null && client.Connected && !string.IsNullOrWhiteSpace(msg))
             {
-                string message = msg;
                 byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, msg);
-                networkStream.Write(packet, 0, packet.Length);                
+                networkStream.Write(packet, 0, packet.Length);
             }
         }
     }
